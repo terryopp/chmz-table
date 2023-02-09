b396d1f64a0b6614c48f568bba03979594948e29
Celioniz <veselovz@inbox.ru>
2023-02-09 11:16:27 +0400

    feat: switcher to first col and id param to row cell

--- src/common/table-data.ts
+++ src/common/table-data.ts
@@ -1,13 +1,19 @@
+const SHOW_FIRST_COL_WITH_ROW_ID = false
+
 const ROWS_AMOUNT = 2000
 const COLS_AMOUNT = 200
 
 const FIRST_COL_ID = 'firstColOffset'
 
-const getIndexName = (index: number) => `key-${index}`;
+const getIndexKeyName = (index: number) => `key-${index}`;
 
 const getContentCell = (index: number) => {
-  const indexedArray = Array.from({ length: COLS_AMOUNT }, (_, key) => getIndexName(key))
-  return indexedArray.reduce((accumulator, value) => ({ ...accumulator, [value]: Math.random().toString(36).slice(2, 12) }), { [FIRST_COL_ID]: `Row ${index + 1}` }) 
+  const indexedArray = Array.from({ length: COLS_AMOUNT }, (_, key) => getIndexKeyName(key))
+  const initialAccumulator = SHOW_FIRST_COL_WITH_ROW_ID ? { [FIRST_COL_ID]: `Row ${index + 1}` } : {}
+  return indexedArray.reduce((accumulator, value) => (
+      { ...accumulator, [value]: Math.random().toString(36).slice(2, 12), id: `row-${index + 1}` + value }),
+      initialAccumulator
+    ) 
 }
 
 export const getContent = () => 
@@ -15,12 +21,12 @@ export const getContent = () =>
 
 export const getColumns = () => {
   const cols = Array.from({ length: COLS_AMOUNT }, (_, index) => {
-    const indexName = getIndexName(index)
+    const indexName = getIndexKeyName(index)
     return { title: `Column ${index + 1}`, key: indexName, dataKey: indexName, width: 150 }
   })
-  const firstCol = { title: '', key: FIRST_COL_ID, dataKey: FIRST_COL_ID, width: 100, frozen: 'left' }
-  return [
-    firstCol,
-    ...cols
-  ]
+  if (SHOW_FIRST_COL_WITH_ROW_ID) {
+    const firstCol = { title: '', key: FIRST_COL_ID, dataKey: FIRST_COL_ID, width: 100, frozen: 'left' }
+    cols.unshift(firstCol)
+  }
+  return cols
 }
